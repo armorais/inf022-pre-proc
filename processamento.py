7ad099bb7c34ee6a5b71b767630e11f699b74d86
Alison Morais <armorais01@gmail.com>
2019-08-28 14:23:33 -0300

    Adicionado leitura simples de xls com xlrd

--- processamento.py
+++ processamento.py
@@ -1,4 +1,4 @@
-import openpyxl
+import xlrd
 import json
 from itens import Municipio
 from itens import JsonRegioes
@@ -6,9 +6,28 @@ from itens import JsonRegioes
 def obj_dict(obj):
     return obj.__dict__
 
+estados = ['Bahia']
+proc_flag = False
+
+wb = xlrd.open_workbook('./2010/abacate.xls', formatting_info=True)
+
+sheet = wb.sheet_by_index(0)
+
+for i in range(sheet.nrows):
+    cell = sheet.cell(i,0)
+    inf = wb.xf_list[cell.xf_index]
+    if(inf.alignment.indent_level == 0):
+        if(not proc_flag and cell.value in estados):
+            proc_flag = True
+        else:
+            proc_flag = False
+    if(proc_flag):
+        print(cell.value)
+        print(inf.alignment.indent_level)
+
 mun1 = Municipio(1,'salvador',13,123123,'lol',[1,2,3,4],[1,2,9,20],12,13)
 mun2 = Municipio(2,'abaira',13,3123,'lol',[1,2,3,4],[5,6,7,8],12,13)
-mun3 = Municipio(2,'abaira',13,3123,'lol',[1,2,3,4],[0,500,0,500],12,13)
+mun3 = Municipio(3,'abaira',13,3123,'lol',[1,2,3,4],[0,500,0,500],12,13)
 
 j = JsonRegioes()
 
@@ -19,14 +38,10 @@ j.add_item("MUNICIPIOS",mun3)
 s = json.dumps(j.get_itens(), default=obj_dict)
 
 try:
-    # Get a file object with write permission.
-
     text_file = open("Output.json", "w")
     text_file.write("%s" % s)
     text_file.close()
 
-    print('./a.json' + " created. ")
+    print("Json criado.")
 except FileNotFoundError:
-    print('./a.json' + " not found. ")
-
-print(s)
+    print("Json não criado, arquivo não encontrado")
