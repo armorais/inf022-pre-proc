6ddd61f9e309bd865ebe98ff40577b61ac5c771d
Alison Morais <alison@ssa-d-biblio-05.intranet.cefetba.br>
2019-09-10 16:58:56 -0300

    Adicionado criacao de arquivos difirentes por pastas

--- processamento.py
+++ processamento.py
@@ -5,9 +5,12 @@ import pandas as pd
 import numpy as np
 from itens import Estado, Mesorregiao, Microrregiao, Municipio
 
+# Parametros de busca
+estado = {'nome':'Bahia', 'sigla':'ba'}
+anos = [2010,2016]
+produto = 'banana'
+
 # Arquivos utilizados
-json_out_file = 'out.json'
-xls_search_file = './2016/banana.xls'
 json_search_file = 'bahia.json'
 
 # Mapeamento das colunas do xls
@@ -20,11 +23,8 @@ VALOR = {"nome":"Valor","coluna":5}
 # Atributos a povoar o json de saída
 atributos = [AREA_DESTINADA["nome"], AREA_COLHIDA["nome"], QUANTIDADE_PRODUZIDA["nome"], VALOR["nome"]]
 
-# Estados a serem incluídos
-estados = ['Bahia']
-
 # Percentis a serem considerados
-percentis_range = [20.0,40.0,60.0,80.0]
+percentis_range = [0.0,40.0,60.0,100.0]
 percentis = []
 
 # Transforma um obj em um dicionário
@@ -51,81 +51,89 @@ def getByValue(dic, valor):
 with open(json_search_file, 'r', encoding='utf8') as f:
     json_estado = json.load(f)
 
-# Abre o xls de busca para leitura
-wb = xlrd.open_workbook(xls_search_file, formatting_info=True)
-
-# Seleciona a primeira página do xls
-sheet = wb.sheet_by_index(0)
-
-# Salva o nome do produto (Categoria)
-nome_produto = sheet.cell(5,0).value
-
 # Instancia o objeto de referência para criação do json
 j = Estado()
 
 # flag utilizada para controlar quando transformar um linha em objeto
 proc_flag = False
 
-# Faz o processamento de cada linha
-for i in range(sheet.nrows):
-    cell = sheet.cell(i,0)
-    inf = wb.xf_list[cell.xf_index]
-    if(inf.alignment.indent_level == 0):
-        if(not proc_flag and cell.value in estados):
-            proc_flag = True
-        else:
-            proc_flag = False
-    if(proc_flag):
-        nome_regiao = sheet.cell(i,NOME_REGIAO["coluna"]).value
-        area_destinada = sheet.cell(i,AREA_DESTINADA["coluna"]).value
-        area_colhida = sheet.cell(i,AREA_COLHIDA["coluna"]).value
-        quantidade_produzida = sheet.cell(i,QUANTIDADE_PRODUZIDA["coluna"]).value
-        valor = sheet.cell(i,VALOR["coluna"]).value
-        valores = [area_destinada,area_colhida,quantidade_produzida,valor]
-        if(inf.alignment.indent_level == 3):
-            nome_regiao = corrigir_nome(nome_regiao)
-            mun = getByValue(json_estado["municipios"],nome_regiao)
-            try:
-                id = mun["id"]
-            except TypeError:
-                print(nome_regiao)
-            id_micro = mun["microrregiao"]["id"]
-            id_meso = mun["microrregiao"]["mesorregiao"]["id"]
-            mun = Municipio(id,nome_regiao,nome_produto,atributos,valores,id_meso,id_micro)
-            j.add_item("MUNICIPIOS",mun)
-        elif(inf.alignment.indent_level == 2):
-            micro = getByValue(json_estado["microrregioes"],nome_regiao)
-            id = micro["id"]
-            id_meso = micro["mesorregiao"]["id"]
-            micro = Microrregiao(id,nome_regiao,nome_produto,atributos,valores,id_meso)
-            j.add_item("MICRORREGIOES",micro)
-        elif(inf.alignment.indent_level == 1):
-            meso = getByValue(json_estado["mesorregioes"],nome_regiao)
-            id = meso["id"]
-            meso = Mesorregiao(id,nome_regiao,nome_produto,atributos,valores)
-            j.add_item("MESORREGIOES",meso)
+for ano in anos:
+    xls_search_file = './'+ str(ano) +'/'+ produto +'.xls'
+    json_out_file =  estado['sigla'] + '_' + str(ano) + '_geral.json';
 
-dataframe = pd.DataFrame.from_records(j.get_valores_list(), columns = atributos)
+    # Abre o xls de busca para leitura
+    wb = xlrd.open_workbook(xls_search_file, formatting_info=True, on_demand=True)
 
-# Pega os percentis e adiciona em uma lista de dicionarios contendo {nome_atributo:percentil}
-for coluna in range(len(atributos)):
-    percentile = []
-    for valor in range(len(percentis_range)):
-        percentile.append(np.percentile(dataframe[atributos[coluna]],percentis_range[valor],0))
-    percentis.append({atributos[coluna]:percentile})
+    # Seleciona a primeira página do xls
+    sheet = wb.sheet_by_index(0)
 
-# Seta os percentis
-j.set_percentis(percentis)
+    # Salva o nome do produto (Categoria)
+    nome_produto = sheet.cell(5,0).value
 
-# Transforma o objeto em json
-s = json.dumps(j.get_itens(), default=obj_dict, ensure_ascii=False)
+    # Faz o processamento de cada linha
+    for i in range(sheet.nrows):
+        cell = sheet.cell(i,0)
+        inf = wb.xf_list[cell.xf_index]
+        if(inf.alignment.indent_level == 0):
+            if(not proc_flag and cell.value in estado['nome']):
+                proc_flag = True
+            else:
+                proc_flag = False
+        if(proc_flag):
+            nome_regiao = sheet.cell(i,NOME_REGIAO["coluna"]).value
+            area_destinada = sheet.cell(i,AREA_DESTINADA["coluna"]).value
+            area_colhida = sheet.cell(i,AREA_COLHIDA["coluna"]).value
+            quantidade_produzida = sheet.cell(i,QUANTIDADE_PRODUZIDA["coluna"]).value
+            valor = sheet.cell(i,VALOR["coluna"]).value
+            valores = [area_destinada,area_colhida,quantidade_produzida,valor]
+            if(inf.alignment.indent_level == 3):
+                nome_regiao = corrigir_nome(nome_regiao)
+                mun = getByValue(json_estado["municipios"],nome_regiao)
+                try:
+                    id = mun["id"]
+                except TypeError:
+                    print(nome_regiao)
+                id_micro = mun["microrregiao"]["id"]
+                id_meso = mun["microrregiao"]["mesorregiao"]["id"]
+                mun = Municipio(id,nome_regiao,nome_produto,atributos,valores,id_meso,id_micro)
+                j.add_item("MUNICIPIOS",mun)
+            elif(inf.alignment.indent_level == 2):
+                micro = getByValue(json_estado["microrregioes"],nome_regiao)
+                id = micro["id"]
+                id_meso = micro["mesorregiao"]["id"]
+                micro = Microrregiao(id,nome_regiao,nome_produto,atributos,valores,id_meso)
+                j.add_item("MICRORREGIOES",micro)
+            elif(inf.alignment.indent_level == 1):
+                meso = getByValue(json_estado["mesorregioes"],nome_regiao)
+                id = meso["id"]
+                meso = Mesorregiao(id,nome_regiao,nome_produto,atributos,valores)
+                j.add_item("MESORREGIOES",meso)
 
-# Salva o json de saída
-try:
-    with io.open(json_out_file, 'w', encoding='utf8') as f:
-        f.write("%s" % s)
-        f.close()
+    # Libera o arquivo xls
+    wb.release_resources()
 
-    print("Json criado.")
-except FileNotFoundError:
-    print("Json nao criado, arquivo nao encontrado")
+    # Carrega os valores obtidos em um DataFrame do pandas, passando os atributos como nome das colunas
+    dataframe = pd.DataFrame.from_records(j.get_valores_list(), columns = atributos)
+
+    # Pega os percentis e adiciona em uma lista de dicionarios contendo {nome_atributo:percentil}
+    for coluna in range(len(atributos)):
+        percentile = []
+        for valor in range(len(percentis_range)):
+            percentile.append(np.percentile(dataframe[atributos[coluna]],percentis_range[valor],0))
+        percentis.append({atributos[coluna]:percentile})
+
+    # Seta os percentis
+    j.set_percentis(percentis)
+
+    # Transforma o objeto em json
+    s = json.dumps(j.get_itens(), default=obj_dict, ensure_ascii=False)
+
+    # Salva o json de saída
+    try:
+        with io.open(json_out_file, 'w', encoding='utf8') as f:
+            f.write("%s" % s)
+            f.close()
+
+        print("Json " + json_out_file + "criado.")
+    except FileNotFoundError:
+        print("Json nao criado, arquivo nao encontrado")
